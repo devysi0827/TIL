45ae9c80617c03da0b624d2129522ea80d0d6ed4
devysi0827 <devysi0827@gmail.com>
2022-09-19 22:16:33 +0900

    [algo] leetcode17

---
+++ Algorithm/문제/study/leetcode17.py
@@ -0,0 +1,25 @@
+digits = ""
+alpabet = [
+            ["a","b","c"],
+            ["d","e","f"],
+            ["g","h","i"],
+            ["j","k","l"],
+            ["m","n","o"],
+            ["p","q","r","s"],
+            ["t","u","v"],
+            ["w","x","y","z"]
+        ]
+n = len(digits)
+answer = []
+def three_bits(word):
+    order = len(word)
+    if order == n:
+        answer.append(word)
+    else:
+        now = int(digits[order]) -2
+        for i in range(len(alpabet[now])):
+            new_word = word
+            new_word += alpabet[now][i]
+            three_bits(new_word)
+three_bits("")
+print(answer)
