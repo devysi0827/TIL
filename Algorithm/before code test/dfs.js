707ecd82ff0d5d4a906a17a4bc7a2df5a5c297ea
devysi0827 <devysi0827@gmail.com>
2023-05-28 00:14:48 +0900

    [algo] dfs loop && rec

--- Algorithm/before code test/dfs.js
+++ Algorithm/before code test/dfs.js
@@ -3,8 +3,10 @@ const n = 6
 const adj = [[0,1,1,0,0,0],[1,0,0,1,1,0],[1,0,0,0,0,1],[0,1,0,0,0,0],[0,1,0,0,0,1],[0,0,1,0,1,0]]
 const order = [] // 순서 찾기 문제 변형
 
-// dfs logic
+// dfs
 const visited = {}
+
+// dfs logic by rec
 function dfs(node) {
     visited[node] = 1
     order.push(node)
@@ -15,8 +17,22 @@ function dfs(node) {
     }
 }
 
-dfs(0)
+// dfs(0)
 
-while (stack.length){
+// dfs logic by loop
+const stack = []
+stack.push(0)
 
+while (stack.length > 0){
+    const node = stack.pop()
+    if (!visited[node]) {
+        visited[node] = 1
+        order.push(node)
+    }
+    for (let i=0; i<n; i++){
+        if (adj[node][i] === 1 && !visited[i]){
+            stack.push(i)
+        }
+    }
 }
+console.log(order)
