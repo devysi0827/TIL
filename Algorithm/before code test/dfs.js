8b2ae0d43fb8fd2eb0d57ad136953c07372642cc
devysi0827 <devysi0827@gmail.com>
2023-06-13 13:46:13 +0900

    [algo] dfs

--- Algorithm/before code test/dfs.js
+++ Algorithm/before code test/dfs.js
@@ -1,23 +1,25 @@
-// problem
-const n = 6
-const adj = [[0,1,1,0,0,0],[1,0,0,1,1,0],[1,0,0,0,0,1],[0,1,0,0,0,0],[0,1,0,0,0,1],[0,0,1,0,1,0]]
-const order = [] // 순서 찾기 문제 변형
-
-// dfs
+// 노드의 개수, 노드간의 연결관계(인접행렬), 방문여부 확인 visited, 순서를 프린트할 order
+const n = 7
+const adj = [[0,1,1,0,1,0,0],[0,0,0,1,0,1,0],[0,0,0,0,0,0,1],[0,0,0,0,0,0,0],[0,0,0,1,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
 const visited = {}
+const order = []
 
-// dfs logic by rec
-function dfs(node) {
-    visited[node] = 1
-    order.push(node)
-    for (let i=0; i<n; i++){
-        if (adj[node][i] === 1 && !visited[i]){
-            dfs(i)
-        }
-    }
-}
 
+// // dfs logic by rec
+// function dfs(node) {
+//     visited[node] = 1
+//     order.push(node+1)
+//     for (let i=0; i<n; i++){
+//         if (adj[node][i] === 1 && !visited[i]){
+//             dfs(i)
+//         }
+//     }
+// }
+//
 // dfs(0)
+// console.log(order)
+
+
 
 // dfs logic by loop
 const stack = []
@@ -27,7 +29,7 @@ while (stack.length > 0){
     const node = stack.pop()
     if (!visited[node]) {
         visited[node] = 1
-        order.push(node)
+        order.push(node+1)
     }
     for (let i=0; i<n; i++){
         if (adj[node][i] === 1 && !visited[i]){
